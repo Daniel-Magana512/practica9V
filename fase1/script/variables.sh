5523632a6f4dae25e830b146107331e9d15daec0
Daniel Magaña <danielmp01189@gmail.com>
2022-12-12 19:09:39 +0000

    actualizar

--- fase1/script/variables.sh
+++ fase1/script/variables.sh
@@ -2,3 +2,9 @@ DB_HOST_PRIVATE_IP=172.31.88.137
 DB_NAME=db_name
 DB_USER=db_user
 DB_PASS=db_pass
+
+WP_HOME=https://practicafas2dmp.ddns.net
+WP_SITEURL=https://practicafas2dmp.ddns.net/wordpress
+
+EMAIL=chema51226@gmail.com
+DOMAIN=practicafas2dmp.ddns.net
