1ecea61fb855f8015698d3a14103025521a1b8df
Daniel Magaña <danielmp01189@gmail.com>
2022-12-12 22:10:53 +0000

    actualizar

--- fase1/script/variables.sh
+++ fase1/script/variables.sh
@@ -3,8 +3,8 @@ DB_NAME=db_name
 DB_USER=db_user
 DB_PASS=db_pass
 
-WP_HOME=https://practica9dmpscri.ddns.net
-WP_SITEURL=https://practica9dmpscri.ddns.net/wordpress
+WP_HOME=https://practica9verdad.ddns.net
+WP_SITEURL=https://practica9verdad.ddns.net/wordpress
 
 EMAIL=chema51226@gmail.com
-DOMAIN=practica9dmpscri.ddns.net
+DOMAIN=practica9verdad.ddns.net
