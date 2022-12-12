76705d4188e2bf323696f4148ce59cf58f9ffe5c
Daniel <danielmp01189@gmail.com>
2022-12-12 11:53:15 +0000

    actualizar

--- fase0/script/variables.sh
+++ fase0/script/variables.sh
@@ -3,8 +3,8 @@ DB_NAME=db_name
 DB_USER=db_user
 DB_PASS=db_pass
 
-WP_HOME=http://practica9realdmp.ddns.net
-WP_SITEURL=http://practica9realdmp.ddns.net/wordpress
+WP_HOME=http://practica9verdmp.ddns.net
+WP_SITEURL=http://practica9verdmp.ddns.net/wordpress
 
 EMAIL=chema51226@gmail.com
-DOMAIN=practica9realdmp.ddns.net
+DOMAIN=practica9verdmp.ddns.net
