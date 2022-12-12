49a12dc6c69be93161fce0f04587614a53b68dfe
Daniel Magaña <danielmp01189@gmail.com>
2022-12-12 23:03:22 +0000

    actualización

---
+++ fase2/script/install_nfs_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set - x
+apt update
+
+# Instalación nfs server
+apt install nfs-kernel-server -y
+
+#Creamos el directorio que queramos compartir
+mkdir -p /var/www/html
+
+# COnfiguramos el propietario y el grupo del direcorio que quiero compartir
+chown nobody:nogroup /var/www/html
+
+#Pasamos el archivo de conf a /etc/exports
+cp ./conf/exports /etc/exports
+
+#Reiniciamos el servidor nfs
+systemclt restart nfs-kernel-server
