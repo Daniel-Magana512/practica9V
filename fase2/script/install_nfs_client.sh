146d2043126636c2e324dc314adaa0ab9cd92d5d
Daniel Magaña <danielmp01189@gmail.com>
2022-12-13 00:23:18 +0000

    actualizar

--- fase2/script/install_nfs_client.sh
+++ fase2/script/install_nfs_client.sh
@@ -10,3 +10,6 @@ apt install nfs-common -y
 
 #Montamos NFS SERVER en la máquina cliente
 mount $NFS_SERVER_IP_PRIVATE:/var/www/html /var/www/html
+
+#Editamos el archivo /etc/fstab para que al iniciar la máquina se monte automáticamente el directorio compartido por NFS.
+echo "$NFS_SERVER_IP_PRIVATE:/var/www/html /var/www/html  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> -a /etc/fstab
